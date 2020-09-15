b2ff420f0aeef1096b964315f9a290d24119abf8
KRL1991 <kimx4706@edu.easj.dk>
2020-09-15 11:15:28 +0200

    Exercise 2.3 and 2.4 added

--- src/Kapitel_2_exercises/Exercise_2_3.java
+++ src/Kapitel_2_exercises/Exercise_2_3.java
@@ -1,4 +1,27 @@
 package Kapitel_2_exercises;
 
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Exercise_2_3 {
-}
+
+    public static void main(String[] args) {
+        //Create Scanner object
+        Scanner input = new Scanner(System.in);
+
+
+        //Promt the user to enter meters
+        System.out.print(" Enter meters ");
+        double meters = input.nextDouble();
+
+        //Compute area
+        double feettometers = ( meters * 3.2786);
+
+        //Display result
+        System.out.println(meters+" meters is "+feettometers+ " feet " );
+    }
+
+
+    }
