d8f44ab30b356eb82aa87533da35def235ae603d
KRL1991 <kimx4706@edu.easj.dk>
2020-09-15 09:20:17 +0200

    Started exercises from the programming book

---
+++ src/Kapitel_2/Chp_02_Programming_Exercises.java
@@ -0,0 +1,24 @@
+package Kapitel_2;
+import java.util.Scanner; //Scanner Import
+
+public class Chp_02_Programming_Exercises {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercise 2.1");
+
+        //Create Scanner object
+        Scanner input = new Scanner(System.in);
+
+        //Promt the user to enter a radius
+        System.out.print(" Enter Miles ");
+        double miles = input.nextDouble();
+
+        //Compute miles
+
+
+
+
+
+    }
+}
